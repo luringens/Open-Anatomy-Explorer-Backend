b40d43074a6288ffd6a003bf49c72116c6649f59
Stian Soltvedt <stian@soltvedt.info>
2020-11-11 13:36:36 +0100

    Failed manual CORS

--- readme.md
+++ readme.md
@@ -5,14 +5,7 @@
 This backend must be configured through environment variables. For example:
 
 ```txt
+MODELS_DIR=./models
 LABEL_DATA_DIR=./data-labels
 QUIZ_DATA_DIR=./data-quiz
-```
-
-## Building
-
-To get live reloading, run:
-
-```sh
-systemfd --no-pid -s http::3000 -- cargo watch -x run
 ```
--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ mod util;
 
 fn main() {
     dotenv::dotenv().ok();
-    const REQUIRED_ENV: [&'static str; 2] = ["QUIZ_DATA_DIR", "LABEL_DATA_DIR"];
+    const REQUIRED_ENV: [&'static str; 3] = ["QUIZ_DATA_DIR", "LABEL_DATA_DIR", "MODELS_DIR"];
     for var in REQUIRED_ENV.iter() {
         if let Err(_) = std::env::var(var) {
             eprintln!("{} not found in environment variables.", var);
@@ -24,6 +24,38 @@ fn main() {
             "/LabelPoints",
             routes![labelpoint::create, labelpoint::load, labelpoint::delete],
         )
-        .mount("/models", StaticFiles::from("/models"))
+        .mount(
+            "/models",
+            StaticFiles::from(std::env::var("MODELS_DIR").unwrap()).rank(-1),
+        )
+        .mount("/models", routes![models_index])
+        .attach(Cors {})
         .launch();
+}
+
+#[rocket::get("/")]
+pub fn models_index() -> Result<rocket_contrib::json::Json<Vec<String>>, Box<dyn std::error::Error>>
+{
+    let data_dir = std::env::var("MODELS_DIR").unwrap();
+    let paths: Vec<_> = std::fs::read_dir(data_dir)?
+        .into_iter()
+        .filter_map(|f| f.ok().and_then(|d| d.file_name().into_string().ok()))
+        .collect();
+
+    Ok(rocket_contrib::json::Json(paths))
+}
+
+struct Cors {}
+
+impl rocket::fairing::Fairing for Cors {
+    fn info(&self) -> rocket::fairing::Info {
+        rocket::fairing::Info {
+            name: "CORS header fairing",
+            kind: rocket::fairing::Kind::Response,
+        }
+    }
+
+    fn on_response(&self, _: &rocket::Request, response: &mut rocket::Response) {
+        response.adjoin_raw_header("Access-Control-Allow-Origin", "*");
+    }
 }
