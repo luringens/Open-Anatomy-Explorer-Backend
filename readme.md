f1a4f3faa9c44fdbdd66a34bf17d6f470881b00c
Stian Soltvedt <stian@soltvedt.info>
2020-11-11 14:31:18 +0100

    Stricter CORS, more comments

--- readme.md
+++ readme.md
@@ -8,4 +8,5 @@ This backend must be configured through environment variables. For example:
 MODELS_DIR=./models
 LABEL_DATA_DIR=./data-labels
 QUIZ_DATA_DIR=./data-quiz
+CORS=http://example.com
 ```
--- src/main.rs
+++ src/main.rs
@@ -9,22 +9,40 @@ mod quiz;
 mod util;
 
 fn main() {
+    // Load environment variable from `.env` if present
     dotenv::dotenv().ok();
-    const REQUIRED_ENV: [&'static str; 3] = ["QUIZ_DATA_DIR", "LABEL_DATA_DIR", "MODELS_DIR"];
-    for var in REQUIRED_ENV.iter() {
-        if let Err(_) = std::env::var(var) {
-            eprintln!("{} not found in environment variables.", var);
+
+    // Confirm all required environment variables are present and the directories exist.
+    {
+        const REQUIRED_ENV: [&'static str; 3] = ["QUIZ_DATA_DIR", "LABEL_DATA_DIR", "MODELS_DIR"];
+        let mut missing_env = false;
+        for env in REQUIRED_ENV.iter() {
+            if let Ok(path) = std::env::var(env) {
+                if let Err(e) = std::fs::create_dir_all(path) {
+                    missing_env = true;
+                    eprintln!("Path for '{}' could not be created: {:?}", env, e);
+                }
+            } else {
+                missing_env = true;
+                eprintln!("Missing environment variable '{}'", env);
+            }
+        }
+        if missing_env {
             return;
         }
     }
 
-    let cors = rocket_cors::CorsOptions {
-        allowed_origins: rocket_cors::AllowedOrigins::all(),
-        ..Default::default()
+    // Set up CORS as this API will be called from other pages.
+    let mut allowed_origins = vec!["http://localhost:*".to_owned()];
+    if let Ok(cors) = std::env::var("CORS") {
+        allowed_origins.push(cors);
     }
-    .to_cors()
-    .expect("Failed to initialize CORS.");
+    let cors = rocket_cors::CorsOptions::default()
+        .allowed_origins(rocket_cors::AllowedOrigins::some_regex(&allowed_origins))
+        .to_cors()
+        .expect("Failed to initialize CORS.");
 
+    // Mount paths and cors fairing and launch the application.
     rocket::ignite()
         .mount(
             "/Quiz",
@@ -41,13 +59,14 @@ fn main() {
         )
         .mount(
             "/models",
-            StaticFiles::from(std::env::var("MODELS_DIR").unwrap()).rank(-1),
+            StaticFiles::from(std::env::var("MODELS_DIR").unwrap()).rank(isize::max_value()),
         )
         .mount("/models", routes![models_index])
         .attach(cors)
         .launch();
 }
 
+/// Overrides models index with a json list of models.
 #[rocket::get("/")]
 pub fn models_index() -> Result<rocket_contrib::json::Json<Vec<String>>, Box<dyn std::error::Error>>
 {
